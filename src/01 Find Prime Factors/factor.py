ded390ca5b007e70b33227c725e274473f2f5cc9
Keshini Patel <34700360+Keshpatel@users.noreply.github.com>
2025-10-21 19:57:16 +0000

    prime Number all -01

--- src/01 Find Prime Factors/factor.py
+++ src/01 Find Prime Factors/factor.py
@@ -31,7 +31,7 @@ def prime_factors(n):
     # Step 1: Handle factor 2 separately
    while n % 2 == 0:
      factors.append(2)
-     n //=2
+     n //=2             # divide by 2 until it’s no longer divisible
    
     # Step 2: Now check for odd factors from 3 up to sqrt(number)
    i=3
@@ -57,19 +57,39 @@ def get_prime_factors(number):
             divisor += 1
     return factors
 
+def get_primeFromRange(startNum, endNum):
+  print(f"\nPrime numbers between {startNum} and {endNum} are:")  
+  for n in range(startNum, endNum + 1):
+    #If number is greater then 1 then  Number can be prime .
+      if n > 1 : 
+          is_prime = True
+      # verify divisibility by numbers from 2 up to its square root
+          for i in range(2, int(n**0.5) + 1):
+            if n % i == 0 :
+              is_prime = False
+              break
+          if is_prime:
+              print(n, end=" ") 
+
 if __name__ == '__main__':
-    print(is_prime_Check(2))
-    print(is_prime_Check(3))
-    print(is_prime_Check(7))
-    print(is_prime_Check(10))
-    print(is_prime_Check(14))
-    print(is_prime_Check(18))
-    print(is_prime_Check(19))
-    print(is_prime_Check(51))
+    # print("Is 2 prime number? ", is_prime_Check(2))
+    # print("Is 13 prime number? ", is_prime_Check(13))
+    # print("Is 130 prime number? ", is_prime_Check(130))
+    # print("Is 7 ?", is_prime_Check(7))
+    # print("Is 10 ?", is_prime_Check(10))
+    # print("Is 14 ?", is_prime_Check(14))
+    # print("Is 200 ?",is_prime_Check(200))
+    # print("Is 678 ?", is_prime_Check(678))
+    # print("Is 51 ?", is_prime_Check(51))
 
-    print("Prime Factors Of 160 are : " , prime_factors(160))
-    print("Prime Factors Of 13 are : " , prime_factors(13))
-    print("Prime Factors Of 360 are : " , prime_factors(630))
+    # print("Prime Factors Of 160 are : " , prime_factors(160))
+    # print("Prime Factors Of 13 are : " , prime_factors(13))
+    # print("Prime Factors Of 360 are : " , prime_factors(630))
 
-    print(get_prime_factors(630))  # [2, 3, 3, 5, 7] 
-    print(get_prime_factors(13))  # [13]
+    # print(get_prime_factors(630))  # [2, 3, 3, 5, 7] 
+    # print(get_prime_factors(13))  # [13]
+    get_primeFromRange(2, 40)
+    get_primeFromRange(41, 70)
+    get_primeFromRange(50, 100)
+    get_primeFromRange(1, 100)
+    
