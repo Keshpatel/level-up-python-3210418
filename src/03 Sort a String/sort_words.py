577063dc19218f0265d979431dcd7a92041abd73
Keshini Patel <34700360+Keshpatel@users.noreply.github.com>
2025-10-22 14:17:29 +0000

    Sort_And_Index 01

--- src/03 Sort a String/sort_words.py
+++ src/03 Sort a String/sort_words.py
@@ -1,3 +1,12 @@
+def sort_wordsAlphatically():
+    user_input = input("Enter any String to Arrange in Alphabetical order . ")
+    words = user_input.lower().split()
+    words.sort()
+    print("Words in alphabetical orer : ")
+    for word in words:
+        print(word)
+
+
 def sort_words(words):
     return ' '.join(sorted(words.split(), key=str.casefold))
 
@@ -5,3 +14,4 @@ def sort_words(words):
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(sort_words('banana ORANGE apple'))  # apple banana ORANGE
+    sort_wordsAlphatically()
--- src/04 Find All List Items/index_all.py
+++ src/04 Find All List Items/index_all.py
@@ -1,3 +1,22 @@
+def find_Index_Of(nested_list, target_value):
+    results = []
+    # Inner recursive function
+    def search(sublist, current_path):
+        for index, item in enumerate(sublist):
+            # Build the path as we go deeper
+            new_path = current_path + [index]
+            # If item matches the target, add the path
+            if item == target_value:
+                results.append(new_path)
+            # If item is a list, search inside recursively
+            elif isinstance(item, list):
+                search(item, new_path)
+    # Start the search from the top level
+    search(nested_list, [])
+    return results
+       
+
+
 def index_all(search_list, item):
     index_list = []
     for index, value in enumerate(search_list):
@@ -14,3 +33,8 @@ if __name__ == '__main__':
     example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
     print(index_all(example, 2))  # [[0, 0, 1], [0, 1], [1, 1]]
     print(index_all(example, [1, 2, 3]))  # [[0, 0], [1]]
+
+    nestedList_data = [1, [2,3,4], [1, 2, 3], 2, [11, 22, 30], 3 , [5, 4, 30], 4 ]
+    print(find_Index_Of(nestedList_data, 1))
+    print(find_Index_Of(nestedList_data, 4))
+    
